97488207b290ce13ecc4670921a50e12ba1ea544
Yuchen Ying <yegle@uga.edu>
2013-01-23 16:17:44 -0500

    Add: Python version of lexer

---
+++ spring2013/csci6570-compiler/pylexer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import re
+import sys
+from ctypes import *
+
+lexer = cdll.LoadLibrary("./libtjlex.so")
+
+# Python2/3 compatible functions
+def _b(text):
+    if sys.version_info[0] == 2:
+        return bytes(text)
+    else:
+        return bytes(text, 'utf-8')
+
+def yylex(filename):
+    filename = _b(filename)
+    print(filename)
+    ret = lexer.set_file(filename)
+    if ret == -1:
+        return
+    while True:
+        tok = lexer.yylex()
+        if tok:
+            yield (tok, c_char_p.in_dll(lexer, 'yytext').value)
+        else:
+            break
+
+def get_symbols(header_file):
+    d = dict()
+    pattern = r'[ \t]+'
+    for line in open(header_file):
+        line = re.sub(pattern, ' ', line)
+        _, symbol, value = line.strip().split(' ')
+        d[int(value)] = symbol
+    return d
+
+if __name__ == '__main__':
+    symbols = get_symbols('y.tab.h')
+    for token in yylex('sample.java'):
+        s = symbols[token[0]]
+        text = token[1].decode('utf-8')
+        print('%s "%s"' % (s, text))
