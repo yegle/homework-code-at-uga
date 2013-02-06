3f1e02d325a08bdfe27ed9553d7d3d6c8d995bcb
Yuchen Ying <yegle@uga.edu>
2013-02-06 12:16:47 -0500

    Mod: bind to multiple local address

--- spring2013/csci6760-computer-networks/geventtest.py
+++ spring2013/csci6760-computer-networks/geventtest.py
@@ -5,21 +5,34 @@ from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 
 from gevent import socket, pool
+from collections import Counter
 
-def make_connection(address):
+
+
+def make_connection(local_addr, remote_addr):
     s = socket.socket()
-
-    s.connect(address)
-    return s
+    s.bind(local_addr)
+    s.connect(remote_addr)
+    global l
+    l.append(s)
+    global c
+    c['count'] +=1
 
 if __name__ == '__main__':
-    c = 0
+    c = Counter()
     p = pool.Pool()
+    l = list()
+    localaddr = [
+        '127.0.0.1',
+        '127.0.0.2',
+        '127.0.0.3',
+        '127.0.0.4',
+        '127.0.0.5',
+    ]
     while True:
-        p.spawn(make_connection, ('localhost',9000))
+        p.spawn(make_connection, (localaddr[c['count']%5], 0), ('127.0.0.1',9000))
         try:
             p.join(raise_error=True)
-            c += 1
         except Exception:
-            print(c)
+            print(c['count'])
             break
