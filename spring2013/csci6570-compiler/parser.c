3e649498899be1bcfbed5a1125a7c1a122360b6f
Yuchen Ying <yegle@uga.edu>
2013-02-13 10:31:43 -0500

    Mod: refactory code

--- spring2013/csci6570-compiler/parser.c
+++ spring2013/csci6570-compiler/parser.c
@@ -1,11 +1,35 @@
 #include<stdio.h>
 #include<string.h>
+#include<errno.h>
+
+#include "y.tab.h"
+#include "check_filename.h"
 
 extern FILE *yyin;
-extern yydebug;
+extern int yydebug;
+
+void usage(){
+    fprintf(stderr, "Usage: prog TinyJavaProgram.java\n");
+    return;
+}
 
 int main(int argc, char* argv[]){
+    if(argc < 2){
+        usage();
+        return -1;
+    }
+    if (!check_filename(argv[1])){
+        fprintf(stderr, "Error: the filename should end with .java\n");
+        usage();
+        return -2;
+    }
+    yyin = fopen(argv[1], "r");
+    if(yyin == NULL){
+        fprintf(stderr, "Error opening file. Reason: %s\n",
+                strerror(errno));
+        usage();
+        return -2;
+    }
     yydebug = 1;
-    yyin = fopen("sample.java", "r");
     yyparse();
 }
--- spring2013/csci6570-compiler/tjc.c
+++ spring2013/csci6570-compiler/tjc.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<errno.h>
 #include<string.h>
-#include<stdbool.h>
+
 #include "y.tab.h"
+#include "check_filename.h"
 
 
 extern FILE *yyin;
@@ -13,27 +14,6 @@ extern char *reason;
 void usage(){
     fprintf(stderr, "Usage: prog TinyJavaProgram.java\n");
     return;
-}
-
-_Bool check_filename(const char* filename){
-    int check_len = sizeof(".java");
-    char *str = ".java";
-
-    int str_len = strlen(str);
-    int filename_len = strlen(filename);
-
-    if (filename_len < check_len){
-        return false;
-    }
-
-    bool ret = true;
-    for (int i=0; i<check_len; i++){
-        if(str[str_len-i] != filename[filename_len-i]){
-            ret = false;
-            break;
-        }
-    }
-    return ret;
 }
 
 int main(int argc, char* argv[]){
