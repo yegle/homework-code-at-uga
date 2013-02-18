860b0f71a87c890bd35801cceae7d264ba20be8d
Yuchen Ying <yegle@uga.edu>
2013-02-17 20:53:41 -0500

    Mod: I don't know what the hell is going on...

---
+++ spring2013/csci8730-advanced-operating-system/02-e1000-kernel-module/e1000.h
@@ -0,0 +1,64 @@
+#include <linux/tcp.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+
+#define E1000_GET_DESC(R, i, type)	(&(((struct type *)((R).desc))[i]))
+#define E1000_RX_DESC(R, i)		E1000_GET_DESC(R, i, e1000_rx_desc)
+#define E1000_TX_DESC(R, i)		E1000_GET_DESC(R, i, e1000_tx_desc)
+#define E1000_CONTEXT_DESC(R, i)	E1000_GET_DESC(R, i, e1000_context_desc)
+struct e1000_buffer {
+	struct sk_buff *skb;
+	dma_addr_t dma;
+	struct page *page;
+	unsigned long time_stamp;
+	u16 length;
+	u16 next_to_watch;
+	unsigned int segs;
+	unsigned int bytecount;
+	u16 mapped_as_page;
+};
+
+struct e1000_tx_ring {
+	/* pointer to the descriptor ring memory */
+	void *desc;
+	/* physical address of the descriptor ring */
+	dma_addr_t dma;
+	/* length of descriptor ring in bytes */
+	unsigned int size;
+	/* number of descriptors in the ring */
+	unsigned int count;
+	/* next descriptor to associate a buffer with */
+	unsigned int next_to_use;
+	/* next descriptor to check for DD status bit */
+	unsigned int next_to_clean;
+	/* array of buffer information structs */
+	struct e1000_buffer *buffer_info;
+
+	u16 tdh;
+	u16 tdt;
+	bool last_tx_tso;
+};
+
+struct e1000_rx_ring {
+	/* pointer to the descriptor ring memory */
+	void *desc;
+	/* physical address of the descriptor ring */
+	dma_addr_t dma;
+	/* length of descriptor ring in bytes */
+	unsigned int size;
+	/* number of descriptors in the ring */
+	unsigned int count;
+	/* next descriptor to associate a buffer with */
+	unsigned int next_to_use;
+	/* next descriptor to check for DD status bit */
+	unsigned int next_to_clean;
+	/* array of buffer information structs */
+	struct e1000_buffer *buffer_info;
+	struct sk_buff *rx_skb_top;
+
+	/* cpu for rx queue */
+	int cpu;
+
+	u16 rdh;
+	u16 rdt;
+};
--- spring2013/csci8730-advanced-operating-system/02-e1000-kernel-module/netdev.c
+++ spring2013/csci8730-advanced-operating-system/02-e1000-kernel-module/netdev.c
@@ -12,6 +12,9 @@
 #include <linux/etherdevice.h>
 #include <linux/pci.h>
 
+#include "e1000.h"
+#include "e1000_hw.h"
+
 #define BAR_0       0
 #define BAR_1       1
 #define BAR_5       5
@@ -22,6 +25,17 @@ char modname[] = "netdev";
 
 struct net_device *my_netdev;
 
+/* private field */
+struct ethx_priv
+{
+    struct pci_dev *pdev;
+    void *mmio_addr;
+    unsigned long reg_len;
+    struct e1000_tx_ring* tx_ring;
+    struct e1000_rx_ring* rx_ring;
+    void *bar;
+};
+
 static int ethx_init(struct net_device *net_dev);
 static int ethx_open(struct net_device *net_dev);
 static int ethx_stop(struct net_device *net_dev);
@@ -31,6 +45,9 @@ static int ethx_ioctl(struct net_device *net_dev, struct ifreq *ifr, int cmd);
 static void ethx_uninit(struct net_device *dev);
 static struct net_device_stats* ethx_get_stats(struct net_device *dev);
 static struct pci_dev* ethx_probe(struct pci_dev *pdev);
+static void ethx_set_rx_mode(struct net_device *netdev);
+static int __devinit ethx_alloc_queues(struct ethx_priv *pp);
+static void ethx_configure_tx(struct ethx_priv *pp);
 
 static const struct net_device_ops my_dev_ops = {
     .ndo_init = ethx_init,
@@ -40,15 +57,8 @@ static const struct net_device_ops my_dev_ops = {
     .ndo_get_stats = ethx_get_stats,
     .ndo_start_xmit = ethx_xmit_frame,
     .ndo_uninit = ethx_uninit,
-    .ndo_change_mtu = ethx_change_mtu
-};
-
-/* private field */
-struct ethx_priv
-{
-    struct pci_dev *pdev;
-    void *mmio_addr;
-    unsigned long reg_len;
+    .ndo_change_mtu = ethx_change_mtu,
+    .ndo_set_rx_mode = ethx_set_rx_mode,
 };
 
 /*******************************************************
@@ -78,7 +88,11 @@ static struct pci_dev* ethx_probe(struct pci_dev *pdev)
 
 static int ethx_init(struct net_device *dev)
 {
+    struct ethx_priv *pp;
     printk(KERN_INFO "-->INSIDE ethx_init");
+    pp = netdev_priv(dev);
+    ethx_alloc_queues(pp);
+    ethx_configure_tx(pp);
     return 0;
 }
 
@@ -110,18 +124,84 @@ static int ethx_stop(struct net_device * netdev)
 
 static int ethx_xmit_frame(struct sk_buff *skb, struct net_device *net_dev)
 {
+	struct e1000_context_desc *context_desc;
+	struct e1000_buffer *buffer_info;
+	unsigned int i;
+	u32 cmd_length = 0;
+	u16 ipcse = 0, tucse, mss;
+	u8 ipcss, ipcso, tucss, tucso, hdr_len;
+	//int err;
+    struct ethx_priv *pp;
+    struct e1000_tx_ring *tx_ring;
 
     printk(KERN_INFO "-->INSIDE ethx_xmit_frame");
+
+    pp = netdev_priv(net_dev);
+
+    tx_ring = pp->tx_ring;
+
 
     /* update dev states */
     net_dev->stats.tx_packets += 1;
     net_dev->stats.tx_bytes += skb->len;
 
-    /* free skb */
-    if (skb)
-        dev_kfree_skb(skb);
 
-    return 0;
+    ///* free skb */
+    //if (skb)
+    //    dev_kfree_skb(skb);
+
+    //return NETDEV_TX_OK;
+
+    hdr_len = skb_transport_offset(skb) + tcp_hdrlen(skb);
+    mss = skb_shinfo(skb)->gso_size;
+    if (skb->protocol == htons(ETH_P_IP)) {
+        struct iphdr *iph = ip_hdr(skb);
+        iph->tot_len = 0;
+        iph->check = 0;
+        tcp_hdr(skb)->check = ~csum_tcpudp_magic(iph->saddr,
+                iph->daddr, 0,
+                IPPROTO_TCP,
+                0);
+        cmd_length = E1000_TXD_CMD_IP;
+        ipcse = skb_transport_offset(skb) - 1;
+    } else if (skb->protocol == htons(ETH_P_IPV6)) {
+        ipv6_hdr(skb)->payload_len = 0;
+        tcp_hdr(skb)->check =
+            ~csum_ipv6_magic(&ipv6_hdr(skb)->saddr,
+                    &ipv6_hdr(skb)->daddr,
+                    0, IPPROTO_TCP, 0);
+        ipcse = 0;
+    }
+    ipcss = skb_network_offset(skb);
+    ipcso = (void *)&(ip_hdr(skb)->check) - (void *)skb->data;
+    tucss = skb_transport_offset(skb);
+    tucso = (void *)&(tcp_hdr(skb)->check) - (void *)skb->data;
+    tucse = 0;
+
+    cmd_length |= (E1000_TXD_CMD_DEXT | E1000_TXD_CMD_TSE |
+            E1000_TXD_CMD_TCP | (skb->len - (hdr_len)));
+
+    i = tx_ring->next_to_use;
+    context_desc = E1000_CONTEXT_DESC(*tx_ring, i);
+    buffer_info = &tx_ring->buffer_info[i];
+
+    context_desc->lower_setup.ip_fields.ipcss  = ipcss;
+    //context_desc->lower_setup.ip_fields.ipcso  = ipcso;
+    //context_desc->lower_setup.ip_fields.ipcse  = cpu_to_le16(ipcse);
+    //context_desc->upper_setup.tcp_fields.tucss = tucss;
+    //context_desc->upper_setup.tcp_fields.tucso = tucso;
+    //context_desc->upper_setup.tcp_fields.tucse = cpu_to_le16(tucse);
+    //context_desc->tcp_seg_setup.fields.mss     = cpu_to_le16(mss);
+    //context_desc->tcp_seg_setup.fields.hdr_len = hdr_len;
+    //context_desc->cmd_and_length = cpu_to_le32(cmd_length);
+
+    //buffer_info->time_stamp = jiffies;
+    //buffer_info->next_to_watch = i;
+
+    //if (++i == tx_ring->count) i = 0;
+    //tx_ring->next_to_use = i;
+
+    return true;
 }
 
 static int ethx_change_mtu(struct net_device *net_dev, int new_mtu)
@@ -148,7 +228,7 @@ int get_mod_info(char *buf, char **start, off_t off, int count, int *eof, void *
 {
     printk(KERN_INFO "-->INSIDE get_mod_info");
     return 0;
-}	
+}
 
 struct proc_dir_entry *proc_file;
 
@@ -245,6 +325,7 @@ static int __init my_netdev_init(void)
     my_netdev->base_addr = (long)ioaddr;
     printk(KERN_INFO "my_netdev->base_addr %lx",(long)ioaddr);
     pp->mmio_addr = ioaddr;
+    pp->bar = pci_ioremap_bar(pdev, BAR_0);
     printk(KERN_INFO "pp->mmio_addr %p",ioaddr);
     pp->reg_len = mmio_len;
 
@@ -276,6 +357,7 @@ static int __init my_netdev_init(void)
         return -1;
     }
 
+
     proc_file = create_proc_entry(modname, 0644, NULL);
     proc_file->read_proc = get_mod_info;
     return ret;
@@ -299,6 +381,60 @@ static void __exit my_netdev_exit(void)
     printk(KERN_INFO "-->INSIDE netdev exit");
     return;
 }
+
+static void ethx_set_rx_mode(struct net_device *netdev){
+    struct ethx_priv *pp;
+    long* ioaddr;
+    u32 rctl;
+    pp = netdev_priv(netdev);
+    ioaddr = pp->mmio_addr;
+    rctl = readl(pp->bar + E1000_RCTL);
+    // XXX: some modification to rctl here
+    writel(rctl, pp->bar + E1000_RCTL);
+}
+
+static int __devinit ethx_alloc_queues(struct ethx_priv *pp)
+{
+    struct pci_dev *pdev = pp->pdev;
+    int size;
+    printk(KERN_INFO "-->ALLOCATING ring");
+	pp->tx_ring = kcalloc(1, sizeof(struct e1000_tx_ring), GFP_KERNEL);
+	if (pp->tx_ring)
+		return -ENOMEM;
+
+	pp->rx_ring = kcalloc(1, sizeof(struct e1000_rx_ring), GFP_KERNEL);
+	if (!pp->rx_ring) {
+		kfree(pp->tx_ring);
+		return -ENOMEM;
+	}
+
+    size = sizeof(struct e1000_buffer) * pp->tx_ring->count;
+
+    pp->tx_ring->buffer_info = vzalloc(size);
+    if (!pp->tx_ring->buffer_info){
+        printk(KERN_INFO "cannot allocate tx_ring buffer");
+        return -ENOMEM;
+    }
+    memset(pp->tx_ring->desc, 0, pp->tx_ring->size);
+    pp->tx_ring->next_to_use = 0;
+    pp->tx_ring->next_to_clean = 0;
+
+    return 0;
+}
+
+static void ethx_configure_tx(struct ethx_priv *pp){
+    u32 tctl;
+    printk(KERN_INFO "-->ethx_configure_tx");
+
+    tctl = readl(pp->bar + E1000_TCTL);
+
+    tctl |= E1000_TCTL_EN;
+    tctl |= E1000_TCTL_PSP;
+    //tctl &= E1000_TCTL_CT;
+
+    writel(tctl, pp->bar + E1000_TCTL);
+}
+
 
 module_init(my_netdev_init);
 module_exit(my_netdev_exit);
