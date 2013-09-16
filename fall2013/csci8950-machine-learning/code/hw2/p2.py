f5ed7a4f48164441e1b8caec0ed29752f61a5b2f
yegle <yegle@ifanr.com>
2013-09-16 01:13:57 -0400

    Mod: Assignment 2 document

--- fall2013/csci8950-machine-learning/code/hw2/p2.py
+++ fall2013/csci8950-machine-learning/code/hw2/p2.py
@@ -6,6 +6,7 @@ from __future__ import (unicode_literals, absolute_import,
 
 
 import csv
+import time
 from sklearn import tree
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cross_validation import KFold
@@ -39,22 +40,29 @@ if __name__ == '__main__':
             tree.export_graphviz(clf, out_file=output_file,
                                  feature_names=vec.get_feature_names())
 
-        kf = KFold(len(training_set), n_folds=2)
-        score = 0
-        for train_indices, test_indices in kf:
-            clf = tree.DecisionTreeClassifier(criterion='entropy',
-                                             random_state=1)
+        kf = KFold(len(training_set), n_folds=10)
 
-            vec = DictVectorizer()
-            X_train = [training_set[i] for i in train_indices]
-            Y_train = [target_set[i] for i in train_indices]
+        for split in range(2, 11):
+            #print("split=%d" % (split))
+            score = 0
+            start = time.time()
+            for train_indices, test_indices in kf:
+                clf = tree.DecisionTreeClassifier(criterion='entropy',
+                                                  random_state=1, min_samples_split=split)
 
-            vectorized_training_set = vec.fit_transform(X_train)
+                vec = DictVectorizer()
+                X_train = [training_set[i] for i in train_indices]
+                Y_train = [target_set[i] for i in train_indices]
 
-            X_test = [training_set[i] for i in test_indices]
-            vectorized_X_test = vec.transform(X_test)
-            Y_test = [target_set[i] for i in test_indices]
+                vectorized_training_set = vec.fit_transform(X_train)
 
-            clf.fit(vectorized_training_set.toarray(), Y_train)
-            score += clf.score(vectorized_X_test.toarray(), Y_test)
-        print(score/10)
+                X_test = [training_set[i] for i in test_indices]
+                vectorized_X_test = vec.transform(X_test)
+                Y_test = [target_set[i] for i in test_indices]
+
+                clf.fit(vectorized_training_set.toarray(), Y_train)
+                s = clf.score(vectorized_X_test.toarray(), Y_test)
+                score += s
+            end = time.time()
+            #print("time cost = %s" % (end-start))
+            print(score/10)
